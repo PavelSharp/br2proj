11d29f98e2c1dedd8b70dbaeb7fda9cb3b1f0c80
PavelSharp <pavel.ivonov@icloud.com>
2026-09-19 23:47:23 +0300

    SKB has been fully reversed and documented
    Successfully parsed every single .skb file from BR2GOC and BR2Cut

--- br2proj/skb.py
+++ br2proj/skb.py
@@ -13,7 +13,7 @@ from .sern.fixed_types import *
 
 @fixed_dataclass
 class SKB_Header:
-    verison:int = sernAs(c_int32)
+    version:int = sernAs(c_int32) #12
     numBones:int = sernAs(c_int32)
 
 @sern_dataclass
@@ -27,39 +27,39 @@ class SKB_Bone:
 
 
 @fixed_dataclass
-class SKB_Unk2:
-    a:int = sernAs(c_int16)
-    flag:int = sernAs(c_int16)
-    b:int = sernAs(c_int32)
+class SKB_Signal:
+    frame:int = sernAs(c_int16) #[EXPI 19.09.2026]
+    type:int = sernAs(c_int16) #[EXPI 19.09.2026]
+    sound_id:int = sernAs(c_int32) #[EXPI 19.09.2026] if type=1 then sound index from SKB_Anim.sounds; if type=0 then this global id sound. #TODO Find the places where the engine builds this table
 
-@fixed_dataclass    
-class SKB_Unk4:
-    a:int = sernAs(c_int32)
-    b:float = sernAs(c_float)
-    c:float = sernAs(c_float)
+@fixed_dataclass
+class SKB_Range:
+    flag_ind:int = sernAs(c_int32) #[EXPI 19.09.2026]
+    start_frame:float = sernAs(c_float) #[EXPI 19.09.2026]
+    end_frame:float = sernAs(c_float) #[EXPI 19.09.2026]
 
 @sern_dataclass
 class SKB_Anim:
     name: ascii_str = sernAs(ascii_char * 30)
     ani_file_name: ascii_str = sernAs(ascii_char * 64)
-    a:float = sernAs(c_float)             #?? seems 30.0, 32.0, 
+    fps:float = sernAs(c_float)          #[EXPI 19.09.2026] #?? seems 30.0, 32.0,
     numFrames: int = sernAs(c_int32)
-    c:float = sernAs(c_float)           #0.1/0.3 -> frame duration, maybe? ??
-    d:int = sernAs(c_int32)           #?? 0, -1, -2... 426?
-    e:float = sernAs(c_float)           #?? less, but very close to numFrames !!! delta = f(d)
-    f:int = sernAs(c_int32)
+    tween_time:float = sernAs(c_float)   #[EXPI 19.09.2026] Transition time to next animation #0.1/0.3 -> frame duration, maybe? ??
+    next_anim_ind:int = sernAs(c_int32)  #[EXPI 19.09.2026] Special values: -1 = freeze on last frame, -2 = infinite loop #?? 0, -1, -2... 426?
+    play_frames:float = sernAs(c_float)  #[EXPI 19.09.2026] But 0.0 may have a special meaning (Manual/Driven Keyframing). #?? less, but very close to numFrames !!! delta = f(d)
+    mask:int = sernAs(c_int32)           #[EXPI 19.09.2026] bitmask: 1 - mirroring, 4 = Allow cache in engine [see sub_722410]. Tested on run_forward, flag 1 mirrors the start from the right foot to the left foot.
 
-    i1:int = sernAs(c_int32)
-    i1_data:list[ascii_str] = sernAs(list[ascii_char * 64], rarg=KnownArg('i1'))
+    numSounds:int = sernAs(c_int32) #[EXPI 19.09.2026]
+    sounds:list[ascii_str] = sernAs(list[ascii_char * 64], rarg=KnownArg('numSounds')) #[EXPI 19.09.2026]
 
-    i2:int = sernAs(c_int32)
-    i2_data:list[SKB_Unk2] = sernAs(rarg=KnownArg('i2')) #(kframe, smthn?)
+    numSignals:int = sernAs(c_int32) #[EXPI 19.09.2026]
+    signals:list[SKB_Signal] = sernAs(rarg=KnownArg('numSignals')) #[EXPI 19.09.2026] #(kframe, smthn?)
 
-    i3:int = sernAs(c_int32)
-    i3_data:list[int] = sernAs(list[c_int32], rarg=KnownArg('i3'))
+    numMarkers:int = sernAs(c_int32) #[EXPI 19.09.2026]
+    markers:list[int] = sernAs(list[c_int32], rarg=KnownArg('numMarkers')) #[EXPI 19.09.2026] List of specific frames with timeline markers #TODO Find semantic meaning
     
-    i4:int = sernAs(c_int32) #small inaccuracy in [BR2 3D FILE FORMATS DOCUMENT], should be int
-    i4_data:list[SKB_Unk4] = sernAs(rarg=KnownArg('i4'))
+    numRanges:int = sernAs(c_int32) #[EXPI 19.09.2026] #small inaccuracy in [BR2 3D FILE FORMATS DOCUMENT], should be int
+    ranges:list[SKB_Range] = sernAs(rarg=KnownArg('numRanges')) #[EXPI 19.09.2026]
 
     def sern_jwrite(self):
         from dataclasses import asdict
@@ -72,12 +72,6 @@ class SKB_Anim:
         dec('name')
         dec('ani_file_name')
         return di
-
-    #See sub_722410 in Br2GOC for more details
-    #for i in range(dict['i2']):
-    #    val = dict['i2_data'][i]
-    #    if (val.flag==1):
-    #        dict['i2_data'][i] = {'a': val.a, 'flag':val.flag, 'b': dict['i1_data'][val.b]}
 
 @sern_dataclass
 class SKB_File:
@@ -103,6 +97,61 @@ class SKB_File:
             dict |= {'numAnims':0, 'anims':[]}
         #jexplore.jprint(dict, path='test.json')
         return cls(**dict)
+
+#================================
+#            NOTES
+#================================
+# ==[About EXPI vs NEW Labels]==
+# Fields labeled as NEW are derived from reverse engineering the original binary code
+# Fields labeled as EXPI denote that their semantic purpose relies on 
+# empirical confirmation. This validation includes several methods such as:
+# 1 Verifying the hypothesis by scanning all game assets (even for Br2GOC and Br2Cut)
+# 2 Performing target field modifications, and observing the game behavior in response.
+
+# ==[Semantic analysis of SKB_Anim.mask & 4]==
+# A global scan across all BR2GOC and BR2Cut files shows that this flag is set 
+# only for animations involving multiple entities, including pair 
+# fatalities, throw states, and monster feeding tracks.
+# However, according to sub_722410, this is implemented as regular caching.
+
+# ==[The ranges of the following fields were confirmed by scanning all files BR2GOC and BR2Cut]==
+# SKB_Header.version = 12
+# SKB_Anim.next_anim_ind = [-2, -1, ..., SKB_File.numAnims)
+# SKB_Anim.play_frames  = [0...SKB_Anim.numFrames]
+# SKB_Anim.mask = [0, 1, 4], but not for 5 (1|4)
+# SKB_Anim.markers = [0...SKB_Anim.numFrames] (for each)
+# SKB_Signal.type = [0, 1]
+# SKB_Signal.value = [0...SKB_Anim.numSounds) (for each if signal.type=1)
+# SKB_Range.flag_ind = [0...SKB_File.numFlags)
+
+# ==[All possible SKB_File.flags]==
+# After scanning all the game files, the following flags were found:
+# [acro_trans, allow_recovery, allowharpoon, allowturning, attract_victim, bbladedamage, 
+# bite_me, bladedamage1, bladedamage2, bladedamage3, bladedamage4, check_hit_wall, 
+# check_pole_turn, check_release, combocontinue, continueevade, create_beam, fire_left_gun, 
+# fire_right_gun, fire_roach, firegundamage, gundamage2, gundamage3, head_damage, 
+# ignoregravity, jumpTwistLand, lbladedamag2, lbladedamage, lbladedamage1, lbladedamage2, 
+# lbladedamage3, lbladedamage4, lclaw2, lclaw3, lfootdamage, lfootdamage1, 
+# lfootdamage2, lfootdamage3, lfootdamage4, lhand_damage, lhanddamage1, lhanddamage2, 
+# lhanddamage3, lkick3, nogravity,  nosliding, onground, ragecontinue, 
+# rbladedamage, rbladedamage1, rbladedamage2, rbladedamage3, rbladedamage4, rclaw1, 
+# rclaw2, rclaw3, rfootdamage, rfootdamage1, rfootdamage2, rfootdamage3, 
+# rfootdamage4, rhand_damage, rhanddamage1, rhanddamage2, rhanddamage3, staff_blink, 
+# vulnerable, weapondamage1, weapondamage2, weapondamage3]
+
+# ==[Resource Compiler & Leftover Strings Discovery]==
+# BR2 uses null-terminated, fixed-length strings. Interestingly, inside the 
+# SKB binaries, the padding bytes after the null-terminator contain readable 
+# text fragments. These uncleaned chunks allowed us to reconstruct the exact 
+# semantic names of various fields and structures used here (like <markers>).
+#
+# This implies that TRI's internal tools originally generated human-readable 
+# text configuration files, which a resource compiler then baked into fixed 
+# binary streams without clearing the memory buffers. 
+#
+# Consequently, the game engine might still contain legacy parsers for text-based 
+# equivalents. This theory perfectly explains the game's file extension pairings: 
+# text-based formats (SKL, DFM, TIFF) vs. compiled binary formats (SKB, BFM, TEX).
 
 #Легкие анимации
 #Почти все неизвестные поля по нулям, это должен быть хороший знак для начала исследования этого формата
